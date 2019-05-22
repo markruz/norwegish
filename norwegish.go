cb1aa585454c5deb1c020a680f5c0e9aed188283
Josh Judge <josh@joshjudge.org>
2019-05-22 17:55:38 -0500

    Translate capital W to capital V

--- norwegish.go
+++ norwegish.go
@@ -3,6 +3,6 @@ package norwegish
 import "strings"
 
 func Translate(s string) string {
-	r := strings.NewReplacer("o", "ø", "c", "k", "th", "t", "w", "v")
+	r := strings.NewReplacer("o", "ø", "c", "k", "th", "t", "w", "v", "W", "V")
 	return r.Replace(s)
 }
